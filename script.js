e1c7c0cbf59ac00cd48c22ba762459ab0d3c7620
liver-juice <113276155+liver-juice@users.noreply.github.com>
2024-06-20 20:10:26 -0500

    body of the program is set up. we are creating the very "person" object, then calling functions on that object in order to modify it, currently randomly, but eventually dynamically.

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
 
 
-function genFirstName(){
-    return "Larry"
+// \\----|||-THE-STAT-BUILDING-FUNCTIONS-|||----//
+
+
+function genFirstName(gender){
+    let name;
+    // in order to get the name, we need to fetch it from the api.
+    // pass the gender to the api, it goes into the black box and returns us a list of baby names or whatever tf 
+    // then we take that data and make it useable again.
+
+    // so name = callthisapi(with this parameter!)
+    if (gender === "Male"){
+        fetch('https://api.api-ninjas.com/v1/babynames?gender=boy')
+        .then(response => console.log(response))
+        .catch(error => console.error(error));
+    }else{
+        fetch('https://api.api-ninjas.com/v1/babynames?gender=girl');
+    }
 }
-function genLastName(){
-    return "Lord"
+
+
+function genLastName(gender){
+    let name;
+    if (gender === "Male"){
+        return name;
+    }else{
+        return name;
+    }
 }
+
+
 function genGender(){
     let randNum = Math.round(Math.random());
     if (randNum === 0){
@@ -16,6 +40,8 @@ function genGender(){
     }
     
 }
+
+
 function genAge(){
     //shitter system, too many old fellas
     return Math.round(Math.random() * 100);
@@ -26,6 +52,8 @@ function genAge(){
 }
 
 
+
+
 // when button is pressed, call the create person function!
 let buton = document.querySelector("#btn");
 buton.addEventListener("click", createPerson);
@@ -33,8 +61,34 @@ buton.addEventListener("click", createPerson);
 
 // the create person function!
 function createPerson(){
+
+    // the "person" array
     let person = {};
-    person.Gender = genGender();
+
+
+    // generating the buddies attributes. stats. perks. dealing the cards.
+    person.gender = genGender();
     person.age = genAge();
+
+
+    if (person.gender === "Male"){
+        person.firstName = genFirstName("Male");
+        person.lastName = genLastName("Male");
+    }else{
+        person.firstName = genFirstName("Female");
+        person.lastName = genLastName("Female");
+    }
+    
+
+
+    // if this person has ___ stat, they can perform ____ action(method)
+    if (person.age >= 18 && person.age <= 70){
+        // this is afterdetail.
+        // person.job = genJob(); 
+    }
+
+
+
+    // show me this person. i am god. bow to your creator.
     console.log(person);
 }
